60337418dc928dd79ce192169395d69178f95ace
Abhinav Vadrevu <avadrevu@twitter.com>
2013-07-02 14:50:24 -0700

    second try

--- calls/models.py
+++ calls/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
